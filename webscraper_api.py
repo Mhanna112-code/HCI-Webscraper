2660a96779d8d988ef63fe0eac45e261be9222d9
Marc Hanna <Marc.Hanna-1@ou.edu>
2023-04-20 19:49:47 -0500

    Initial backend commit

---
+++ webscraper_api.py
@@ -0,0 +1,55 @@
+import re
+import time
+import datetime
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+
+async def get_data(category):
+    try:
+        # TODO replace query with local html file location
+        query_url = f"https://sfbay.craigslist.org/search/sfc/sss?query={category}#search=1~gallery~0~0"
+
+        # Get html from query url
+        response = requests.get(query_url)
+        page_content = response.content
+
+        # Initialize BeautifulSoup with html content
+        soup = BeautifulSoup(page_content, 'html.parser')
+        # Array to hold data rows
+        search_results = []
+
+        # TODO Change script tags to traverse embedded tags until you get to li tags containing posting info we need
+        # located at Body -> main ->div-class =“cl-content”  ->
+        # main class = “cl-search cl-hide-filters cl-search-view-mode-gallery section-sss category-sss cl-narrow cl-search-sort-mode-relevance" div-class =“cl-search-results” ->
+        # div-class ="results cl-results-page cl-search-view-mode-gallery narrow” ->
+        # ol -> li
+        script_tags = soup.find_all('main')
+        # TODO for loop through li tags to pull data we need
+        for result in script_tags:
+            location_match = re.search(r'location:\s*({.*?})\s*,', result.text)
+            if location_match:
+                location_dict = location_match.group(1)
+                region_match = re.search(r'"region":\s*"([^"]+)"', location_dict)
+                city_match = re.search(r'"city":\s*"([^"]+)"', location_dict)
+                if region_match:
+                    region = region_match.group(1)
+                if city_match:
+                    city = city_match.group(1)
+                location = city + ', ' + region
+                search_results.append([query_url, location])
+        time.sleep(1)
+        columns = (['PostURL', 'Location'])
+        # Store data in dataframe
+        df = pd.DataFrame(search_results, columns=columns)
+        timestamp = datetime.datetime.now().strftime('%m_%d_%y %H%M%S')
+        # Output Dataframe to CSV
+        df.to_csv(f'Craigslist Results ({timestamp}).csv', index=False)
+        print('File Successfully Created!')
+
+    except Exception as e:
+        print(e)
+
+if __name__ == "__main__":
+    get_data("cars")
