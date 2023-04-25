ad62c75c70193d87d4a3e43759945e208576c6ae
Marc Hanna <Marc.Hanna-1@ou.edu>
2023-04-25 01:17:25 -0500

    all data is correctly scraped

--- webscraper_api.py
+++ webscraper_api.py
@@ -10,14 +10,14 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/api/get_data', methods=['GET'])
 
-def get_data(category):
+@app.route('/api/get_data', methods=['GET'])
+def get_data():
     try:
         # Replace query with local html file location
-        #category = request.args.get('category')
-        #if not category:
-        #    return {"error": "Missing category parameter"}, 400
+        category = request.args.get('category')
+        if not category:
+            return {"error": "Missing category parameter"}, 400
 
         # Local html file location
         file_path = f"{category}.html"
@@ -32,76 +32,31 @@ def get_data(category):
         search_results = []
 
         li_tags = soup.find_all('li', attrs={'class': 'cl-search-result cl-search-view-mode-gallery'})
-
+        post_url = f"https://sfbay.craigslist.org/search/sfc/sss?query={category}#search=1~gallery~0~0"
         # TODO-EMMANUEL: for loop through li tags to pull data we need
         for li in li_tags:
-            # Get the Posting Name, Date, Price and Location of each posting
-            # Get the Posting Name, Date, Price and Location of each posting
-            post_id = li.attrs['data-pid']
-            post_title = li.attrs['title']
-            # post_date = li.find('time', {'class': 'result-date'}).get('datetime', '')
-            post_price = post_price = li.find('span', {'class': 'priceinfo'}).text
-            post_location = li.find('div', {'class': 'gallery-card'}).find('div', {'class': 'cl-gallery'}).find('div', {
-                'class': 'gallery-inner'}).find('div', {'class': 'meta'}).text.strip()
+            post_id = li.get('data-pid')
+            post_title = li.get('title')
 
-            if post_location_tag is not None:
-                post_location = post_location_tag.text.strip()
-            else:
-                post_location = 'N/A'
-            search_results.append([post_id, post_title])
-            # Columns for Excel Sheet
-        columns = (['PostID, Post Name'])
+            # Get the Posting Name, Date, Price and Location of each posting
+            try:
+                post_price = li.find('span', {'class': 'priceinfo'}).text
+            except (AttributeError, TypeError):
+                post_price = 'N/A'
+
+            try:
+                meta_info = li.find('div', {'class': 'meta'}).text.split('·')
+                post_date = meta_info[0].strip()
+                location = meta_info[-1].strip()
+            except (AttributeError, TypeError, IndexError):
+                post_date = 'N/A'
+                location = 'N/A'
+
+            search_results.append([post_id, post_title, post_price, post_date, location, post_url])
+        # Columns for Excel Sheet
+        columns = (['PostID', 'PostTitle', 'PostPrice', 'PostDate', 'PostLocation', 'PostURL'])
         # Store data in dataframe
         df = pd.DataFrame(search_results, columns=columns)
-        timestamp = datetime.datetime.now().strftime('%m_%d_%y %H%M%S')
-        # Output Dataframe to CSV
-        df.to_csv(f'Craigslist Results ({timestamp}).csv', index=False)
-        print('File Successfully Created!')
-        #print(jsonify(df.to_dict(orient='records')))
-        #return jsonify(df.to_dict(orient='records'))
-
-    except Exception as e:
-        print(e)
-
-
-@app.route('/api/output_dummy_csv', methods=['GET'])
-def output_dummy_csv():
-    try:
-        # Replace query with local html file location
-        category = request.args.get('category')
-        if not category:
-            return {"error": "Missing category parameter"}, 400
-
-        file_path = f"{category}.html"
-
-        # Read and parse the local html file
-        with open(file_path, 'r', encoding='utf-8') as file:
-            page_content = file.read()
-
-        # Initialize BeautifulSoup with html content
-        soup = BeautifulSoup(page_content, 'html.parser')
-        # Array to hold data rows
-        search_results = []
-
-        script_tags = soup.find_all('script')
-        for result in script_tags:
-            location_match = re.search(r'location:\s*({.*?})\s*,', result.text)
-            if location_match:
-                location_dict = location_match.group(1)
-                region_match = re.search(r'"region":\s*"([^"]+)"', location_dict)
-                city_match = re.search(r'"city":\s*"([^"]+)"', location_dict)
-                if region_match:
-                    region = region_match.group(1)
-                if city_match:
-                    city = city_match.group(1)
-                location = city + ', ' + region
-                query_url = f"https://sfbay.craigslist.org/search/sfc/sss?query={category}#search=1~gallery~0~0"
-                search_results.append([query_url, location])
-
-        columns = (['PostURL', 'Location'])
-        # Store data in dataframe
-        df = pd.DataFrame(search_results, columns=columns)
-        # Create a Results directory if it doesn't exist
         if not os.path.exists('Results'):
             os.makedirs('Results')
 
@@ -115,9 +70,9 @@ def output_dummy_csv():
 
     except Exception as e:
         print(e)
-        return Response("Error: Could not create file.", status=500)
 
 
 if __name__ == "__main__":
-    #app.run(debug=True)
-    get_data("photography")
+    app.run(debug=True)
+    # get_data("photography")
+
